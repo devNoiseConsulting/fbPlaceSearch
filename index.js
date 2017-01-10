dd8397c405436ae9b62eb1bdb4edd8406a481cf2
Michael J. Flynn <flynnmj@devnoise.com>
2017-01-09 19:40:58 -0500

    Previous work
    Committing the work before the repo was created.

---
+++ index.js
@@ -0,0 +1,159 @@
+let {
+  FB,
+  FacebookApiException
+} = require('fb');
+
+const app_id = '1285125588201073';
+const app_secret = '614c3523ef4f487b1ed184fde323c08a';
+
+const breweryApp = FB.extend({
+  appId: app_id,
+  appSecret: app_secret
+});
+
+/*
+fields:
+id
+name
+website
+category
+hours
+location
+current_location
+store_location_descriptor
+parent_page
+single_line_address
+*/
+
+const fields = 'id,name,website,single_line_address,location,current_location,store_location_descriptor,parent_page,category,hours';
+const latLng = '39.95,-75.16';
+const distance = '20000';
+//let brewerySearch = `/search?q=brewery|brewing&type=place&center=${latLng}&distance=${distance}&fields=${fields}`;
+let brewerySearch = `/search?q=brewery&type=place&center=${latLng}&distance=${distance}&fields=${fields}`;
+let brewingSearch = '/search?q=brewing&type=place&center=39.95,-75.16&distance=10000&' + fields;
+
+let places = [];
+
+FB.api('oauth/access_token', {
+  client_id: app_id,
+  client_secret: app_secret,
+  grant_type: 'client_credentials'
+}, function(res) {
+  if (!res || res.error) {
+    console.log(!res ? 'error occurred' : res.error);
+    return;
+  }
+
+  var accessToken = res.access_token;
+  FB.setAccessToken(accessToken);
+
+  // testGet();
+  searchGet(brewerySearch);
+  //testFbId('42338591948');
+});
+
+/*
+GET graph.facebook.com
+  /search?
+    q={your-query}&
+    type={object-type}
+*/
+function testGet() {
+  FB.api('4', function(res) {
+    if (!res || res.error) {
+      console.log(!res ? 'error occurred' : res.error);
+      return;
+    }
+    console.log(res.id);
+    console.log(res.name);
+  });
+}
+
+function testFbId(fbid) {
+  let request = fbid + '?metadata=0&' + fields;
+  FB.api(request, function(res) {
+    if (!res || res.error) {
+      console.log(!res ? 'error occurred' : res.error);
+      return;
+    }
+    console.log(res);
+    /*
+      metadata:
+       { fields:
+    */
+  });
+}
+
+/*
+GET graph.facebook.com
+  /search?
+    q=coffee&
+    type=place&
+    center=37.76,-122.427&
+    distance=1000
+*/
+function searchGet(search) {
+  console.log(search);
+  FB.api(search, function(res) {
+    if (!res || res.error) {
+      console.log(!res ? 'error occurred' : res.error);
+      return;
+    }
+    console.log(res);
+    const newPlace = res.data.filter(place => place.website && place.location.street);
+    places = places.concat(newPlace);
+    //console.log(places);
+    if (res.paging.next) {
+      setTimeout(function() {
+        console.log('Calling next');
+        searchGet(res.paging.next.substr(32));
+      }, 1000);
+    } else {
+      placesToFeatureCollection(places);
+    }
+  });
+}
+
+function placesToFeatureCollection(places) {
+  let featureCollection = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+  featureCollection.features = places.map(placeToFeature);
+  console.log(featureCollection.features.length);
+  //console.log(JSON.stringify(featureCollection, null, 2));
+
+}
+
+function placeToFeature(place) {
+  let feature =     {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            place.location.longitude,
+            place.location.latitude
+          ]
+        },
+        "properties": {
+          "marker-symbol": "beer",
+          "marker-color": "#607D8B",
+          "marker-size": "small",
+          "name": place.name,
+          "address": place.single_line_address,
+          "url": place.website
+        }
+      };
+      return feature;
+}
+
+/*
+FB.api(
+  '/search',
+  'GET',
+  {"q":"brewery|brewing","type":"place","center":"39.95,-75.16","distance":"161000","fields":"id,name,website,single_line_address,location,current_location,store_location_descriptor,parent_page,category,hours"},
+  function(response) {
+      // Insert your code here
+  }
+);
+*/
